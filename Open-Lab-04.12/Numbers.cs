8d2c794b8cdb8a31c34480aaf66531fba1bd746f
Matúš <matusskor@gmail.com>
2020-10-30 15:58:22 +0100

    zmeny

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._12
 {
     public class Numbers
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                if (nums.Contains(i))
+                {
+
+                }
+                else
+                {
+                    a = i;
+                }
+                
+
+            }
+            return a;
         }
     }
 }
